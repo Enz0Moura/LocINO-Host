30b787102a71d7a37e92136456b879217ace25fb
Enzo Moura <mouraenzo21@gmail.com>
2026-06-22 16:22:38 -0300

    Better sender and receiver code.

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -1,16 +1,17 @@
 #include <LocINO/LocINOSerialClient.hpp>
 #include <LocINO/LocINOCLI.hpp>
 #include <LocINO/Types.hpp>
+#include <chrono>
 
 int main() {
     LocINO::LocINOSerialClient client("/dev/ttyACM0", 9600);
     LocINO::EventPacket event;
-
-    client.listenRecord();
-
-    while (client.receiveEvent(event)){
-        LocINO::CLI::printEvent(event);
-        client.listenRecord();
+    while (true){
+        while ((client.receiveEvent(event)) && (event.type == LocINO::CpuEventType::LoRaRx)){
+            LocINO::CLI::printEvent(event);
+            client.listenRecord();
+        }
+        std::chrono::seconds(3);
     }
 
 }
--- src/sender.cpp
+++ src/sender.cpp
@@ -1,18 +1,22 @@
 #include <LocINO/LocINOSerialClient.hpp>
 #include <LocINO/LocINOCLI.hpp>
 #include <LocINO/Types.hpp>
+#include <chrono>
 
 int main() {
     LocINO::LocINOSerialClient client("/dev/ttyACM0", 9600);
     LocINO::EventPacket event;
 
-    auto packet = LocINO::LoRaPacket::fromText("Hello, World!");
-
-    client.sendMessage(packet);
-
-    while (client.receiveEvent(event)){
-        LocINO::CLI::printEvent(event);
+    while (true){
+        
+        auto packet = LocINO::LoRaPacket::fromText("Hello, World!");
         client.sendMessage(packet);
-    }
 
+        while (client.receiveEvent(event)){
+            LocINO::CLI::printEvent(event);
+            if (event.type == LocINO::CpuEventType::LoRaTxOk) break;
+        }
+        std::chrono::seconds(10);
+    
+    }
 }
